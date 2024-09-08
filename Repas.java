ff9ab4021ed10f21082f3a123a18f3e0bda5261d
Wisskaii <wisskaii93@gmail.com>
2024-09-08 21:05:12 +0200

    C3

--- Repas.java
+++ Repas.java
@@ -4,5 +4,10 @@ public class Repas {
 
     private int numero ;
     private LocalDate date ;
-    
+
+    public Repas(int numero, LocalDate date) {
+        this.numero = numero;
+        this.date = date;
+    }
+
 }
